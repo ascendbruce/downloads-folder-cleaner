13b02673bcfc6835fd6510d4ae3f79036ca8f273
Bruce Li <ascendbruce@gmail.com>
2017-03-12 16:09:53 +0800

    Refine output of installation script
    ## Why is this change necessary?
    
    - Easy to read installation output.

--- install.sh
+++ install.sh
@@ -6,14 +6,26 @@ export PLIST_FILE="$PLIST_DIR/net.bruceli.downloads-folder-cleaner.plist"
 mkdir -p $SCRIPT_DIR
 mkdir -p $PLIST_DIR
 
+echo
+echo "========= Downloading =========="
+echo
+
+echo $SCRIPT_FILE
 cd $SCRIPT_DIR
 curl -OfL https://raw.githubusercontent.com/ascendbruce/downloads-folder-cleaner/master/downloads-folder-cleaner.applescript
+echo
 
+echo $PLIST_FILE
 cd $PLIST_DIR
 curl -OfL https://raw.githubusercontent.com/ascendbruce/downloads-folder-cleaner/master/templates/net.bruceli.downloads-folder-cleaner.plist
-echo $SCRIPT_FILE
-echo $PLIST_FILE
 sed -i '' "s#SCRIPT_PATH#$SCRIPT_FILE#" $PLIST_FILE
+
+echo
+echo "===== Setting routine job ======"
+echo
 
 launchctl unload $PLIST_FILE > /dev/null 2>&1
 launchctl load -w $PLIST_FILE
+
+echo
+echo "Finished"
